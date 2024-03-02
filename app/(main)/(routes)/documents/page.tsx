addde9de2f4e957004c4b91cf23ec277fd1ae660
Mohamed Rafik <71353030+BounaderMedRafik@users.noreply.github.com>
2024-03-02 01:41:45 +0100

    Update page.tsx

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div>ProtectedPage</div>;
+  return <div>ProtectedPage( i edited this from phone lmao )</div>;
 };
 
 export default page;
