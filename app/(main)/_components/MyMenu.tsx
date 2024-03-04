333817b03691155db46f4c7f9fbf48062c0da04c
Mohamed Rafik <71353030+BounaderMedRafik@users.noreply.github.com>
2024-03-04 21:46:23 +0100

    4/03/2024 <?> commit

--- app/(main)/_components/MyMenu.tsx
+++ app/(main)/_components/MyMenu.tsx
@@ -14,7 +14,7 @@ import { useUser } from "@clerk/clerk-react";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Button } from "@/components/ui/button";
-import { MoreHorizontal, Trash } from "lucide-react";
+import { MoreHorizontal, Rabbit, Trash } from "lucide-react";
 import { Skeleton } from "@/components/ui/skeleton";
 interface MenuProps {
   documentId: Id<"documents">;
@@ -24,6 +24,8 @@ const MyMenu = ({ documentId }: MenuProps) => {
   const router = useRouter();
   const { user } = useUser();
   const archive = useMutation(api.documents.archive);
+  const restore = useMutation(api.documents.restore);
+
   const onArchive = () => {
     const promise = archive({ id: documentId });
 
@@ -31,6 +33,18 @@ const MyMenu = ({ documentId }: MenuProps) => {
       loading: "Moving to trash...",
       success: "Note moved to trash!",
       error: "Failed to archive note.",
+    });
+
+    router.push("/documents");
+  };
+
+  const onRestore = () => {
+    const promise = restore({ id: documentId });
+
+    toast.promise(promise, {
+      loading: "Restoring note...",
+      success: "Note restored!",
+      error: "Failed to restore note.",
     });
 
     router.push("/documents");
@@ -52,6 +66,10 @@ const MyMenu = ({ documentId }: MenuProps) => {
         <DropdownMenuItem onClick={onArchive}>
           <Trash className=" h-4 w-4 mr-2" />
           Delete
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={onRestore}>
+          <Rabbit className=" h-4 w-4 mr-2" />
+          RestoreBunny
         </DropdownMenuItem>
         <DropdownMenuSeparator />
         <div className="text-xs text-muted-foreground">
--- components/MyToolBar.tsx
+++ components/MyToolBar.tsx
@@ -1,6 +1,11 @@
+import { Doc } from "@/convex/_generated/dataModel";
 import React from "react";
+interface ToolbarProps {
+  initialData: Doc<"documents">;
+  preview?: boolean;
+}
 
-const MyToolBar = () => {
+const MyToolBar = ({ initialData, preview }: ToolbarProps) => {
   return <div>MyToolBar</div>;
 };
 
